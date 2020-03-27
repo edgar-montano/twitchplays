65b8737b13f1fbb5f0853be72a3194e5916131d5
Edgar Montano <em2532@nyu.edu>
2020-03-26 20:59:55 -0400

    initialized project

---
+++ index.js
@@ -0,0 +1,17 @@
+const TwitchBot = require("twitch-bot");
+const config = require("./config.json");
+const client = new TwitchBot(config);
+
+client.on("join", channel => {
+  console.log(`Joined channel: ${channel}`);
+});
+
+client.on("error", err => {
+  console.log(err);
+});
+
+client.on("message", chatter => {
+  if (chatter.message) {
+    //execute logic here
+  }
+});
